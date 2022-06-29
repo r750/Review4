c8d902a18cc1beefd9d24e17725273cef1bb4dec
r750 <57615798+r750@users.noreply.github.com>
2022-06-29 09:58:44 +0530

    Update AddressBookMain.java

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,2 +1,11 @@
-public class AddressBookMain {
+import java.util.LinkedList;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Contact contact1 =new Contact("ABC","DEF","Delhi");
+        Contact contact2 =new Contact("XYZ","QEU","Pune");
+
+    }
 }
